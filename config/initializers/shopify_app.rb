dee1dd27f28f8bf31d4c0ae66acf780f72aedbb2
Klaus Vennik <contacto@klausvennik.com>
2022-10-10 22:07:02 -0300

    host

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -25,7 +25,7 @@ Rails.application.config.after_initialize do
       api_key: ShopifyApp.configuration.api_key,
       api_secret_key: ShopifyApp.configuration.secret,
       api_version: ShopifyApp.configuration.api_version,
-      host_name: URI(ENV.fetch('HOST', 'https://murmuring-tundra-58118.herokuapp.com')).host || '',
+      host_name: URI(ENV.fetch('HOST', 'https://shopiapp-kvm.herokuapp.com')).host || '',
       scope: ShopifyApp.configuration.scope,
       is_private: !ENV.fetch('SHOPIFY_APP_PRIVATE_SHOP', '').empty?,
       is_embedded: ShopifyApp.configuration.embedded_app,
